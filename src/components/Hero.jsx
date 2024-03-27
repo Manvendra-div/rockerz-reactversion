fb3e749e73f4141b43f5034982f09884412315d5
Manvendra Singh <ms1577239@gmail.com>
2024-03-27 19:26:55 +0530

    unavailable added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ import { Transition, Combobox } from "@headlessui/react";
 import ShareCard from "./ShareCard";
 import parse from "html-react-parser";
 import { NumericFormat } from "react-number-format";
+import unavailable_img from "../assets/unavailable.svg"
 import BASE_API from "../BASE_API.js";
 
 const fetchData = async (URL) => {
@@ -388,155 +389,167 @@ const Hero = ({
           </Transition>
         </div>
         <div className="data-container">
-          {!appearSongCard && <div className="h-screen"></div>}
-          <div className="song-container">
-            <Transition
-              show={appearSongCard}
-              className="m-1 lg:w-[35%]"
-              enter="transition-all ease-in-out duration-500 delay-[200ms]"
-              enterFrom="opacity-0 translate-y-6"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition-all ease-in-out duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              {datafromSearchToggle ? (
-                <span className="font-semibold">
-                  Top matches for {finalSearchQuery}
-                </span>
-              ) : (
-                <span className="font-semibold">Top trending</span>
-              )}
-
-              {songSectionData.songs?.length > 0 && (
-                <div className="relative flex flex-col bg-white/20 rounded-xl m-1 shadow-md group overflow-hidden border border-gray-400 overflow-x-hidden select-none">
-                  <img
-                    src={songSectionData.songs[0]?.image[2].url}
-                    className="w-full object-cover transition-transform transform group-hover:scale-105 rounded-t-xl"
-                    alt="Song Image"
-                  />
-                  <div className="lg:ml-5 mt-1 lg:mt-3 p-2 ">
-                    <h3
-                      className={`text-2xl font-semibold mb-2 leading-tight ${
-                        songSectionData.songs[0]?.name.length > 20
-                          ? "group-hover:animate-marquee"
-                          : ""
-                      }`}
-                    >
-                      {`${parse(songSectionData.songs[0]?.name)}`}
-                    </h3>
-                    <p className="text-sm leading-relaxed">
-                      {parse(
-                        typeof songSectionData.songs[0]?.artists === "string"
-                          ? songSectionData.songs[0]?.artists
-                          : songSectionData.songs[0]?.artists.all
-                              .map((name) => name.name)
-                              .join(", ")
-                      )}
-                    </p>
-                  </div>
-                  <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-                    <button
-                      className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition"
-                      onClick={() => {
-                        pushNotificationForLike();
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        className="bi bi-heart"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                      </svg>
-                    </button>
-                    <button
-                      className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition"
-                      onClick={() => {
-                        launchPlayer(songSectionData?.songs[0]);
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="40"
-                        height="40"
-                        fill="currentColor"
-                        className="bi bi-play-circle-fill"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-                      </svg>
-                    </button>
-                    <span className="flex flex-col items-center w-[10%] hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
-                      <CiPlay1 size={20} />
-                      <span className="text-xs">
-                        <NumericFormat
-                          value={songSectionData.songs[0]?.playCount}
-                          displayType={"text"}
-                          allowLeadingZeros
-                          thousandSeparator=","
-                        />
-                      </span>
-                    </span>
-                  </div>
-                </div>
-              )}
-            </Transition>
-            <Transition
-              show={appearSongCard}
-              className="m-1 lg:w-[60%]"
-              enter="transition-all ease-in-out duration-500 delay-[200ms]"
-              enterFrom="opacity-0 translate-y-6"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition-all ease-in-out duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              {datafromSearchToggle ? (
-                songSectionData.songs?.length < 2 ? null : (
-                  <span className="font-semibold">Related songs</span>
-                )
-              ) : (
-                <span className="font-semibold">Songs</span>
-              )}
-              {songSectionData.songs
-                ?.slice(
-                  1,
-                  songSectionData.songs?.length > 5
-                    ? 5
-                    : songSectionData.songs?.length
-                )
-                .map((song, index) => (
-                  <SongCard data={song} key={index} index={index} />
-                ))}
-            </Transition>
-          </div>
-          <Transition
-            show={appearSongCard}
-            className="m-1 mt-5 w-full"
-            enter="transition-all ease-in-out duration-500 delay-[200ms]"
-            enterFrom="opacity-0 translate-y-6"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition-all ease-in-out duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            {datafromSearchToggle ? (
-              songSectionData.albums?.length < 1 ? null : (
-                <span className="font-semibold">Related Albums</span>
-              )
-            ) : (
-              <span className="font-semibold">Trending Albums</span>
-            )}
-            <div className="flex flex-wrap">
-              {songSectionData.albums?.slice(0, 6).map((song, index) => (
-                <AlbumsCard data={song} key={index} index={index} />
-              ))}
+          {!datafromSearchToggle ? (
+            <div className="h-screen flex flex-col justify-start items-center text-center">
+              <img src={unavailable_img} alt="Unavailable" className="w-full md:w-[30%] opacity-20"/>
+              <p>
+                HomePage is disabled for a moment, but wait you can use search
+                and play tracks that suits best to your mood
+              </p>
             </div>
-          </Transition>
+          ) : (
+            <>
+              <div className="song-container">
+                <Transition
+                  show={appearSongCard}
+                  className="m-1 lg:w-[35%]"
+                  enter="transition-all ease-in-out duration-500 delay-[200ms]"
+                  enterFrom="opacity-0 translate-y-6"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition-all ease-in-out duration-300"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  {datafromSearchToggle ? (
+                    <span className="font-semibold">
+                      Top matches for {finalSearchQuery}
+                    </span>
+                  ) : (
+                    <span className="font-semibold">Top trending</span>
+                  )}
+
+                  {songSectionData.songs?.length > 0 && (
+                    <div className="relative flex flex-col bg-white/20 rounded-xl m-1 shadow-md group overflow-hidden border border-gray-400 overflow-x-hidden select-none">
+                      <img
+                        src={songSectionData.songs[0]?.image[2].url}
+                        className="w-full object-cover transition-transform transform group-hover:scale-105 rounded-t-xl"
+                        alt="Song Image"
+                      />
+                      <div className="lg:ml-5 mt-1 lg:mt-3 p-2 ">
+                        <h3
+                          className={`text-2xl font-semibold mb-2 leading-tight ${
+                            songSectionData.songs[0]?.name.length > 20
+                              ? "group-hover:animate-marquee"
+                              : ""
+                          }`}
+                        >
+                          {`${parse(songSectionData.songs[0]?.name)}`}
+                        </h3>
+                        <p className="text-sm leading-relaxed">
+                          {parse(
+                            typeof songSectionData.songs[0]?.artists ===
+                              "string"
+                              ? songSectionData.songs[0]?.artists
+                              : songSectionData.songs[0]?.artists.all
+                                  .map((name) => name.name)
+                                  .join(", ")
+                          )}
+                        </p>
+                      </div>
+                      <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex items-center group-hover:opacity-100 transition justify-evenly">
+                        <button
+                          className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition"
+                          onClick={() => {
+                            pushNotificationForLike();
+                          }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            className="bi bi-heart"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                          </svg>
+                        </button>
+                        <button
+                          className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition"
+                          onClick={() => {
+                            launchPlayer(songSectionData?.songs[0]);
+                          }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="40"
+                            height="40"
+                            fill="currentColor"
+                            className="bi bi-play-circle-fill"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+                          </svg>
+                        </button>
+                        <span className="flex flex-col items-center w-[10%] hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                          <CiPlay1 size={20} />
+                          <span className="text-xs">
+                            <NumericFormat
+                              value={songSectionData.songs[0]?.playCount}
+                              displayType={"text"}
+                              allowLeadingZeros
+                              thousandSeparator=","
+                            />
+                          </span>
+                        </span>
+                      </div>
+                    </div>
+                  )}
+                </Transition>
+                <Transition
+                  show={appearSongCard}
+                  className="m-1 lg:w-[60%]"
+                  enter="transition-all ease-in-out duration-500 delay-[200ms]"
+                  enterFrom="opacity-0 translate-y-6"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition-all ease-in-out duration-300"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  {datafromSearchToggle ? (
+                    songSectionData.songs?.length < 2 ? null : (
+                      <span className="font-semibold">Related songs</span>
+                    )
+                  ) : (
+                    <span className="font-semibold">Songs</span>
+                  )}
+                  {songSectionData.songs
+                    ?.slice(
+                      1,
+                      songSectionData.songs?.length > 5
+                        ? 5
+                        : songSectionData.songs?.length
+                    )
+                    .map((song, index) => (
+                      <SongCard data={song} key={index} index={index} />
+                    ))}
+                </Transition>
+              </div>
+              <Transition
+                show={appearSongCard}
+                className="m-1 mt-5 w-full"
+                enter="transition-all ease-in-out duration-500 delay-[200ms]"
+                enterFrom="opacity-0 translate-y-6"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition-all ease-in-out duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                {datafromSearchToggle ? (
+                  songSectionData.albums?.length < 1 ? null : (
+                    <span className="font-semibold">Related Albums</span>
+                  )
+                ) : (
+                  <span className="font-semibold">Trending Albums</span>
+                )}
+                <div className="flex flex-wrap">
+                  {songSectionData.albums?.slice(0, 6).map((song, index) => (
+                    <AlbumsCard data={song} key={index} index={index} />
+                  ))}
+                </div>
+              </Transition>
+            </>
+          )}
         </div>
       </div>
     </>
